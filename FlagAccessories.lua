e9649e58fe1607af59f3fde892cdb9807f46965d
MianReplicate <docgoosenn@gmail.com>
2026-07-18 21:36:13 -0700

    Updated pack with new framework standards

--- FlagAccessories.lua
+++ FlagAccessories.lua
@@ -2,20 +2,9 @@ local mutatorName = "FlagAccessories"
 behaviour(mutatorName)
 
 function FlagAccessories:Awake()
-	self.frameworkName = "Custom Flag Framework"
 	self.dataContainer = self.gameObject.GetComponent(DataContainer)
 	self.cover = self.dataContainer.GetTexture("Cover")
 	self.CustomMeshes = {}
-	self.name = mutatorName
-end
-
-function FlagAccessories:Start()
-	local obj = GameObject.Find(self.frameworkName)
-	if(obj) then
-		self.framework = ScriptedBehaviour.GetScript(obj)
-	else
-		error("Framework was not found! Please make sure the Custom Flags Framework mutator has been enabled.")
-	end
 
 	local customMeshes = self.dataContainer.GetGameObjectArray("Mesh")
 
@@ -23,5 +12,6 @@ function FlagAccessories:Start()
 		local renderer = mesh.GetComponent(SkinnedMeshRenderer)
 		table.insert(self.CustomMeshes, {mesh=renderer.sharedMesh, materials=renderer.materials})
 	end
-	self.framework:addMeshPack(self)
+
+	self.name = mutatorName
 end
